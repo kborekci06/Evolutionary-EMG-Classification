1f2147236be04ced49b5db5da68e413cbf7a0d03
kborekci <kaan@borekci.com>
2025-11-20 11:42:04 -0600

    load emg file function

---
+++ emg_evo_lib_kb/preprocessing.py
@@ -0,0 +1,22 @@
+#%% Imports
+from __future__ import annotations
+from pathlib import Path
+from typing import List, Tuple, Dict
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+#%% Data Load Functions
+def load_emg_file(path, emg_column_names):
+    """
+    Load a single EMG text file into a DataFrame.
+
+    Assumes:
+    - whitespace-delimited
+    - 10 columns: Time, ch1..ch8, Class
+    """
+    df = pd.read_csv(path, delim_whitespace=True, header=None, names=emg_column_names)
+    return df
+
