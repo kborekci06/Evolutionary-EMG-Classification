fcf9b7b3fd510031f85d1df470bb9bf86fb0eb8b
kborekci <kaan@borekci.com>
2025-11-30 22:16:03 -0600

    added control_nn.py to __init__.py

--- src/emg_evo_lib_kb/__init__.py
+++ src/emg_evo_lib_kb/__init__.py
@@ -1,2 +1,5 @@
 from .preprocessing import (load_emg_file, iter_emg_files, segment_gestures, estimate_sampling_rate,
                             channel_features, compute_segment_features, build_feature_dataset)
+from .control_nn import (load_data_and_split, build_mlp_classifier, plot_confusion_matrix, 
+                         train_and_evaluate)
+
